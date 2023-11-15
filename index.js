955d330c2b5ce2d7e0511553467ed668e7ac273e
yeonuk44 <yeonuk44@gmail.com>
2023-11-15 17:43:28 +0900

    # Add the ability to view daily candlesticks for the ticker.

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-const accountsInfo = require("./apis/assets");
-const orderCryptocurrency = require("./apis/order");
+// const accountsInfo = require("./apis/assets");
+// const orderCryptocurrency = require("./apis/order");
 
-console.log("Get Accounts Info: Before ordering Cryptocurrency");
+// console.log("Get Accounts Info: Before ordering Cryptocurrency");
 
-accountsInfo
-  .getAllAccountsInfo()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// accountsInfo
+//   .getAllAccountsInfo()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-console.log("Start ordering crypto");
+// console.log("Start ordering crypto");
 
-orderCryptocurrency
-  .orderCryptocurrency()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// orderCryptocurrency
+//   .orderCryptocurrency()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-console.log("Get Accounts Info: After ordering Cryptocurrency");
+// console.log("Get Accounts Info: After ordering Cryptocurrency");
 
-accountsInfo
-  .getAllAccountsInfo()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// accountsInfo
+//   .getAllAccountsInfo()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+const request = require("request");
+
+const options = {
+  method: "GET",
+  url: "https://api.upbit.com/v1/candles/days?count=1&market=KRW-BTC",
+  headers: { accept: "application/json" },
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
