3b42cdb0d9bb773995c0fa17b74440a6e6f2768d
yeonuk44 <yeonuk44@gmail.com>
2023-11-15 15:45:56 +0900

    # Update orderCryptocurrency API

--- apis/order.js
+++ apis/order.js
@@ -5,3 +5,72 @@ const sign = require("jsonwebtoken").sign;
 const queryEncode = require("querystring").encode;
 const dotenv = require("dotenv");
 dotenv.config();
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY;
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
+const server_url = process.env.UPBIT_OPEN_API_SERVER_URL;
+
+/*
+INFO:
+Request Parameters
+Name	설명	타입
+market *	마켓 ID (필수)	String
+side *	주문 종류 (필수)
+- bid : 매수
+- ask : 매도	String
+volume *	주문량 (지정가, 시장가 매도 시 필수)	NumberString
+price *	주문 가격. (지정가, 시장가 매수 시 필수)
+ex) KRW-BTC 마켓에서 1BTC당 1,000 KRW로 거래할 경우, 값은 1000 이 된다.
+ex) KRW-BTC 마켓에서 1BTC당 매도 1호가가 500 KRW 인 경우, 시장가 매수 시 값을 1000으로 세팅하면 2BTC가 매수된다.
+(수수료가 존재하거나 매도 1호가의 수량에 따라 상이할 수 있음)	NumberString
+ord_type *	주문 타입 (필수)
+- limit : 지정가 주문
+- price : 시장가 주문(매수)
+- market : 시장가 주문(매도)	String
+identifier	조회용 사용자 지정값 (선택)	String (Uniq 값 사용)
+*/
+
+const body = {
+  market: "KRW-BTC",
+  side: "bid",
+  //   volume: "0.01",
+  price: "5000",
+  ord_type: "price",
+};
+
+const query = queryEncode(body);
+
+const hash = crypto.createHash("sha512");
+const queryHash = hash.update(query, "utf-8").digest("hex");
+
+const payload = {
+  access_key: access_key,
+  nonce: uuidv4(),
+  query_hash: queryHash,
+  query_hash_alg: "SHA512",
+};
+
+const token = sign(payload, secret_key);
+
+function orderCryptocurrency() {
+  const options = {
+    method: "POST",
+    url: server_url + "/v1/orders",
+    headers: { Authorization: `Bearer ${token}` },
+    json: body,
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+module.exports = {
+  orderCryptocurrency,
+};
--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const accountsInfo = require("./apis/assets");
+const orderCryptocurrency = require("./apis/order");
+
+console.log("Get Accounts Info: Before ordering Cryptocurrency");
+
+accountsInfo
+  .getAllAccountsInfo()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+console.log("Start ordering crypto");
+
+orderCryptocurrency
+  .orderCryptocurrency()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+console.log("Get Accounts Info: After ordering Cryptocurrency");
 
 accountsInfo
   .getAllAccountsInfo()
