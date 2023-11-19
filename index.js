38c63d0a97067ec02a4e096e69d300928332b448
yeonuk44 <yeonuk44@gmail.com>
2023-11-19 11:16:20 +0900

    # Fix: an issue where the current token's price was not immediately updated in KRW.

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ async function fetchData() {
     let isBalance = getAllAccountsInfo.map((item) => item.balance);
     const idxKRW = await findKRW();
     const idxMainToken = await findMainToken();
-    const limitBalance = 900000;
-    const ratioLowerBand = 1.5;
+    const limitBalance = 500000;
+    const ratioLowerBand = 1.0001;
     const ratioUpperBand = 0.975;
     let isTrading = false;
 
@@ -78,16 +78,6 @@ async function fetchData() {
      * TODO:
      * 내 자산의 기준으로 비교할 때, 보유한 토큰의 KRW 가치도 포함하여 총 자산에서 가용 자산을 분별할 수 있게 해야함.
      */
-    // const limitBalance = isBalance[idxKRW] / 2;
-    // if (isCurrency[idxKRW] === tradingToken.TRADING_TOKEN.krw) {
-    //   console.log(
-    //     `보유중인 ${tradingToken.TRADING_TOKEN.krw}: ${isBalance[idxKRW]}`
-    //   );
-    //   console.log("현재 가용 자산의 금액(KRW): " + limitBalance);
-    // } else {
-    //   console.log(`더 이상 ${tradingToken.TRADING_TOKEN.krw}가 없습니다.`);
-    //   return;
-    // }
     if (isCurrency[idxKRW] === tradingToken.TRADING_TOKEN.krw) {
       console.log(
         `보유중인 ${tradingToken.TRADING_TOKEN.krw}: ${isBalance[idxKRW]}`
@@ -106,27 +96,27 @@ async function fetchData() {
       const minuteFetchData = await getCandlesInfo.getMinuteCandleInfo();
       const minuteCandlePrice = minuteFetchData.map((item) => item.price);
       const minuteCandleTime = minuteFetchData.map((item) => item.time);
-      if (isTrading) {
-        console.log("매매 후 보유중인 자산: ");
-        console.log(getAllAccountsInfo);
-      }
 
-      console.log("====================================================");
-      console.log(
-        minuteCandleTime + " 종가: " + minuteCandlePrice + " 입니다."
-      );
+      if (isTrading) {
+        console.log(`====================================================`);
+        console.log(`매매 후 보유중인 자산: `);
+        console.log(getAllAccountsInfo);
+        isTrading = false;
+      } else {
+        console.log(`====================================================`);
+        console.log(
+          `현재 분봉의 시간은 ${minuteCandleTime} 이고, 가격은 ${minuteCandlePrice} 입니다. [참고] lowerBand: ${lowerBand} , upperBand: ${upperBand}.`
+        );
+      }
       /**
        * INFO:
        * 매매 프로세스
        */
       if (minuteCandlePrice <= lowerBand * ratioLowerBand) {
         // 1. BB의 Lower Band 보다 가격이 낮을 때 구매
-        console.log("====================================================");
+        console.log(`====================================================`);
         console.log(
-          lowerBand * ratioLowerBand +
-            " lowerBand의 가격에 근접한 종가로 현재 종가는 " +
-            minuteCandlePrice +
-            " 입니다."
+          `lowerBand의 가격인 ${lowerBand * ratioLowerBand} 에 근접하였습니다.`
         );
 
         if (limitBalance > isBalance[idxKRW]) {
@@ -139,27 +129,28 @@ async function fetchData() {
           );
           console.log("보유중인 자산: ");
           console.log(getAllAccountsInfo);
-          // 트레이딩 봇 종료
-          // clearInterval(minuteInterval);
         } else if (
           isCurrency[idxKRW] === tradingToken.TRADING_TOKEN.krw &&
           isBalance[idxKRW] > 5000
         ) {
-          console.log("====================================================");
           // 매수 주문
           const bidBody = {
             market: `${tradingToken.TRADING_TOKEN.krw}-${tradingToken.TRADING_TOKEN.mainToken}`,
             side: "bid",
-            price: "42000",
+            price: "50000",
             ord_type: "price",
           };
           await orderCryptocurrency
             .bidOrderCryptocurrency(bidBody)
             .then((result) => {
               console.log(
+                "===================================================="
+              );
+              console.log(
                 `${bidBody.price}${tradingToken.TRADING_TOKEN.krw} 만큼의 ${tradingToken.TRADING_TOKEN.mainToken}를 매수했습니다.`
               );
               console.log(result);
+              isTrading = true;
             })
             .catch((error) => {
               console.log("매매 실패");
@@ -170,36 +161,20 @@ async function fetchData() {
         // 2. BB의 Upper Band 보다 가격이 높을 때 판매
         console.log("====================================================");
         console.log(
-          upperBand * ratioUpperBand +
-            " upperBand의 가격에 근접한 종가로 현재 종가는 " +
-            minuteCandlePrice +
-            " 입니다."
+          `upperBand의 가격인 ${upperBand * ratioUpperBand} 에 근접하였습니다.`
         );
         /**
          * INFO:
          * 토큰의 KRW 가치 환산 Response value가 없어 따로 계산
-         *
-         * TODO:
-         * 현재 토큰의 가격이 KRW와 다르게 바로 갱신되지 않는 네트워크 통신 부분의 이슈가 있음.
-         * 업비트 Open API 고객센터에 메일 문의를 보내놓았고, 네트워크 이슈라면 고치면 되는데 코드상의 문제가 있을 수 있기에 다시 살펴볼 필요 있음.
-         * 네트워크 지연에 따른 문제로 확정된다면 시장가로 한 번에 물량 매도가 되지 않을 것임.
          */
         let currentMainTokentoKRW = isBalance[idxMainToken]
           ? isBalance[idxMainToken] * minuteCandlePrice
           : 0 * minuteCandlePrice;
-        console.log("====================================================");
-        console.log(
-          `현재 내가 가진 토큰의 ${
-            tradingToken.TRADING_TOKEN.krw
-          } 금액은 ${currentMainTokentoKRW}이고, 토큰의 메인 balance는 ${
-            isBalance[idxMainToken] ? isBalance[idxMainToken] : 0
-          } 입니다.`
-        );
+
         if (
           isCurrency[idxMainToken] === tradingToken.TRADING_TOKEN.mainToken &&
           currentMainTokentoKRW > 5000
         ) {
-          console.log("====================================================");
           // 매도 주문
           const askBody = {
             market: `${tradingToken.TRADING_TOKEN.krw}-${tradingToken.TRADING_TOKEN.mainToken}`,
@@ -211,11 +186,15 @@ async function fetchData() {
             .askOrderCryptocurrency(askBody)
             .then((result) => {
               console.log(
+                "===================================================="
+              );
+              console.log(
                 `${currentMainTokentoKRW}만큼의 ${tradingToken.TRADING_TOKEN.mainToken}의 볼륨인 ${askBody.volume}을 시장가 매도했습니다.`
               );
               console.log(result);
-              console.log("보유중인 자산: ");
-              console.log(getAllAccountsInfo);
+              isTrading = true;
+              // 트레이딩 봇 종료: 1회성 전량 시장가 매도
+              clearInterval(minuteInterval);
             })
             .catch((error) => {
               console.log("매매 실패");
@@ -223,7 +202,7 @@ async function fetchData() {
             });
         }
       }
-    }, 3000); //ms 단위, 1000ms to 1s
+    }, 60000); //ms 단위, 1000ms to 1s
   } catch (error) {
     console.error(error);
   }
